81ce82439c3785dcbc5c955b6180b1b4102f8eff
Allen Nelson <anelson@narrativescience.com>
2016-10-27 00:19:17 -0500

    starting to build derivation parser

--- src/Nix/Derivation.hs
+++ src/Nix/Derivation.hs
@@ -1,10 +1,22 @@
 -- | Nix derivations.
 module Nix.Derivation where
 
+import Prelude (read)
 import ClassyPrelude
+import Text.Parsec
+import Text.Parsec.Token
+import qualified Data.HashMap.Strict as H
 
-newtype StorePath = StorePath FilePath
+type Parser a = Parsec [Char] () a
+
+-- | Objects in the nix store.
+newtype StorePath = StorePath Text
   deriving (Show, Eq, Generic)
+
+toFullPath :: FilePath -- ^ Path to the nix store.
+           -> StorePath -- ^ Store path.
+           -> FilePath -- ^ Full path to the store object.
+toFullPath pathToStore (StorePath p) = pathToStore </> unpack p
 
 data Derivation = Derivation {
   derivOutputs :: HashMap Text StorePath,
@@ -23,3 +35,35 @@ data Derivation = Derivation {
   derivSystem :: Text
   -- ^ System the derivation is to be built on.
   } deriving (Show, Eq, Generic)
+
+-- | Parses a string constant.
+parseText :: Parser Text
+parseText = char '"' >> loop [] where
+  loop acc = do
+    let continue c = loop (c:acc)
+    anyChar >>= \case
+      '"' -> return $ pack $ reverse acc
+      '\\' -> anyChar >>= \case
+        'n' -> continue '\n'
+        'r' -> continue '\r'
+        't' -> continue '\t'
+        'b' -> continue '\b'
+        c -> continue c
+      c -> continue c
+
+surround :: Char -> Char -> Parser a -> Parser a
+surround start stop p = char start *> p <* char stop
+
+parseDerivation :: Parser (HashMap Text Text)
+parseDerivation = do
+  string "Derive"
+  surround '(' ')' $ do
+    outs <- surround '[' ']' $ flip sepBy1 (char ',') $ do
+      surround '(' ')' $ do
+        outName <- parseText
+        char ','
+        outPath <- parseText
+        string ",\"\",\"\""
+        return (outName, outPath)
+    return $ H.fromList outs
+--  read <$> many1 digit
