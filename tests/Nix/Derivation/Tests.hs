582da2142107e48f579f8d1dc657bbf5df8e5821
Allen Nelson <anelson@narrativescience.com>
2016-10-27 09:45:17 -0500

    parse derivs in the nix store

--- tests/Nix/Derivation/Tests.hs
+++ tests/Nix/Derivation/Tests.hs
@@ -1,3 +1,11 @@
 module Nix.Derivation.Tests where
 
+import System.Directory (listDirectory)
+import System.Environment (getEnv)
+
 import Nix.Derivation
+
+parseAllDerivs :: IO ()
+parseAllDerivs = do
+  nixStore <- getEnv "NIX_STORE"
+  storeObjs <- listDirectory nixStore
